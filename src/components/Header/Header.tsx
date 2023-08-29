d4e6eb5fe2e5150d11adc6113d26785331c1c72e
AleksTimofeev <timoha144a@gmail.com>
2023-08-30 00:50:38 +0300

    change header links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,11 +4,16 @@ import styles from "./Header.module.css";
 import logo from "../../assets/img/logo.jpg";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import {NavLink} from 'react-router-dom'
+import {NavLink, useLocation} from 'react-router-dom'
 import {Path} from "../../constants/path";
+import {useAppSelector} from "../../redux/store";
 
 
 function Header() {
+
+  const {pathname} = useLocation()
+  const isLogged = useAppSelector(state => state.auth.isLogged)
+
   return (
     // Компонент верхней панели навигации
     <Navbar expand="lg" className="bg-body-tertiary">
@@ -23,13 +28,13 @@ function Header() {
         <Navbar.Collapse id="basic-navbar-nav" className={styles.navItems}>
           <Nav className=" nav me-auto ms-auto">
             {/* Ссылки навигации */}
-            <NavLink
+            {pathname === Path.HOME && <NavLink
               to={''}
               className={styles.tooltip}
               data-tooltip="здесь будет переход на страницу с информацией о приложении"
             >
               <button className='btn btn-secondary'>О нас</button>
-            </NavLink>
+            </NavLink>}
 
             <NavLink
               to={''}
@@ -38,13 +43,38 @@ function Header() {
             >
               <button className='btn btn-secondary'>Задания</button>
             </NavLink>
+            {isLogged && <>
+              <NavLink
+                to={''}
+                className={`d-flex justify-content-end ${styles.tooltip}`}
+                data-tooltip="здесь будет переход на страницу с примерами заданий"
+              >
+                <button className='btn btn-secondary'>Резюме</button>
+              </NavLink>
+              <NavLink
+                to={''}
+                className={`d-flex justify-content-end ${styles.tooltip}`}
+                data-tooltip="здесь будет переход на страницу с примерами заданий"
+              >
+                <button className='btn btn-secondary'>Визитка</button>
+              </NavLink>
+            </>}
           </Nav>
 
           <Nav className="ms-auto">
             {/* Ссылки навигации */}
-            <NavLink to={Path.AUTH}>
-              <button className='btn btn-secondary'>Войти</button>
-            </NavLink>
+            {!isLogged ?
+              <NavLink to={Path.AUTH}>
+                <button className='btn btn-secondary'>Войти</button>
+              </NavLink> :
+              pathname === Path.PROFILE ?
+                <NavLink to={''}>
+                  <button className='btn btn-secondary'>Настройки</button>
+                </NavLink> :
+                <NavLink to={Path.PROFILE}>
+                  <button className='btn btn-secondary'>Мой кабинет</button>
+                </NavLink>
+            }
           </Nav>
         </Navbar.Collapse>
       </Container>
