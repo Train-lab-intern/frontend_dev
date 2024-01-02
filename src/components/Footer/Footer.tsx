42d4e61991b4a2a7579db442bb0f32be73465462
AleksTimofeev <timoha144a@gmail.com>
2024-01-02 22:21:38 +0300

    change search input footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,6 +4,7 @@ import logo from '../../assets/img/logo_footer.png'
 import {NavLink} from "react-router-dom";
 import {githubIcon} from "../../assets/icons/githubIcon";
 import {linkedinIcon} from "../../assets/icons/linkedinIcon";
+import {iconSearch} from "../../assets/icons/iconSearch";
 
 type PropsType = {
   mainPageData: any;
@@ -38,7 +39,9 @@ export const Footer: React.FC<PropsType> = () => {
           <div className={styles.search}>
             <span>Поиск по сайту</span>
             <label>
-              <input type="text" placeholder="введите текст "/>
+              <input type="text" placeholder="введите текст"/>
+              <span className={styles.iconSearch}>{iconSearch}</span>
+
             </label>
           </div>
           <div className={styles.links}>
@@ -65,45 +68,5 @@ export const Footer: React.FC<PropsType> = () => {
         </div>
       </div>
     </footer>
-    // <div className={styles.footer}>
-    //   <Container>
-    //     <Row>
-    //       <Col md="3">
-    //         <div className={styles.right_block}>
-    //           <a href="mailto:train.lab@gmail.com">
-    //             <div className={styles.email}>train.lab@gmail.com</div>
-    //           </a>
-    //
-    //           <div className={`${styles.icons} flex flex-row `}>
-    //             <a href="https://www.linkedin.com/company/train-lab-interns/mycompany/" target='_blank'>
-    //               <img src={linkedin} alt="linkedin"/>
-    //             </a>
-    //             <a href="https://github.com/Train-lab-intern"><img src={github} alt="github"/></a>
-    //           </div>
-    //         </div>
-    //       </Col>
-    //
-    //
-    //       <Col md="6">
-    //         <p className={styles.rights}> Все права защищены. Любое использование либо копирование
-    //           материалов
-    //           и (или) подборки материалов сайта, элементов дизайна и оформления
-    //           допускается лишь с письменного разрешения правообладателя и только
-    //           со ссылкой на источник: URL</p>
-    //       </Col>
-    //
-    //
-    //       <Col md="3">
-    //         <div className={styles.right_block}>
-    //           <a href="src/components/Footer/Footer#" className={styles.tooltip}
-    //              data-tooltip="здесь будет переход на страницу о защите персональных данных">
-    //             <div>Персональные данные</div>
-    //           </a>
-    //           <div>{mainPageData ? mainPageData['1.9'] : ''}</div>
-    //         </div>
-    //       </Col>
-    //     </Row>
-    //   </Container>
-    // </div>
   );
 }
