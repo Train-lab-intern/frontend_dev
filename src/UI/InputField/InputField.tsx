2ea56958989afe685b897c31f400ecbc1b2dd30c
Vlad <vlad.podolianchik@gmail.com>
2024-06-16 22:43:22 +0300

    input test commit

--- src/UI/InputField/InputField.tsx
+++ src/UI/InputField/InputField.tsx
@@ -1,31 +1,63 @@
 import './InputField.css';
+// import { useState } from 'react';
 
-interface InputFieldType {
-  type: string;
-  id: string;
-  name: string;
-  minLength: number;
-  required: boolean;
-  placeholder: string;
+
+interface InputFieldProps {
+    type?: string;
+    name?: string;
+    placeholder: string;
+    className?: string;
+    errorText?: string;
+    isError: boolean;
+    //   value?: string;
+    // eslint-disable-next-line no-unused-vars
+    onChange: (value: string) => void;
 }
 
-export default function InputField({
-  type,
-  id,
-  name,
-  minLength,
-  required,
-  placeholder,
-}: InputFieldType) {
-  return (
-    <input
-      className="input-field"
-      type={type}
-      id={id}
-      name={name}
-      minLength={minLength}
-      required={required}
-      placeholder={placeholder}
-    />
-  );
+// export function InputField: React.FC<InputFieldProps> ({ type, isError, name, placeholder, className, errorText }){
+function InputField({
+    type = 'text',
+    isError = false,
+    name = '',
+    placeholder = '',
+    className = '',
+    onChange,
+    errorText = '' }: InputFieldProps) {
+
+    // const [isPasswordVisible, setPasswordVisible] = useState(false);
+    //   const handleChange = (e: { target: HTMLInputElement }) => {
+    //     const newValue = e.target.value;
+    //   }
+
+    // const togglePasswordVisibility = (): void => {
+    //     setPasswordVisible(!isPasswordVisible);
+    // }
+
+    // if (type === 'password') {
+    //     setPasswordVisible(true);
+    // } else {
+    //     setPasswordVisible(false);
+    // }
+
+    return (
+        <div className="input-group-UI">
+            <input
+                className={`${className} ${isError ? 'error' : ''}`}
+                type={type}
+                id='input-UI'
+                required
+                name={name}
+                placeholder=""
+                onChange={(e) => onChange(e.target.value)}
+            />
+            <label className={`placeholder-label ${isError ? 'error' : ''}`} htmlFor='input-UI'>{placeholder}</label>
+            <div className={`${isError ? 'input-error-show' : 'input-error-hidden'}`}>{errorText}</div>
+            {/* {isPasswordVisible ? (
+                <span className='openEye' onClick={togglePasswordVisibility}></span>
+            ) : (
+                <span className='eyeIconHidden' onClick={togglePasswordVisibility}></span>
+            )} */}
+        </div>
+    );
 }
+export default InputField;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,24 @@ import styles from './Header.module.scss';
 import logo from '../../assets/img/logo.png';
 import { Path } from '../../pages/constants/path';
 import Navigation from '../Navigation/Navigation';
+import InputField from '../../UI/InputField';
 
 export default function Header() {
   const { pathname } = useLocation();
 
   return (
     <header className={styles.header}>
+      <InputField
+        placeholder={'placeholderText'}
+        className={'input-field-UI'}
+        errorText={'ERROR-TEXT'}
+        isError={true}
+        type={'text'}
+        name={''}
+      // onChange={newValue}
+      // value={newValue}
+
+      />
       <div className={styles.container}>
         <div className={styles.inner}>
           <div className={styles.logo}>
