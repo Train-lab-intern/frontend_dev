3315941cf060cf87dbc9cf40cd4906a4d5e8cfbc
lansyfonseka <twinkstriker@gmail.com>
2024-08-18 16:33:13 +0300

    fix: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,45 @@
-import { NavLink, useLocation } from 'react-router-dom';
-import styles from './Header.module.scss';
+import './Header.scss';
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 import { Path } from '../../pages/constants/path';
-import Navigation from '../Navigation/Navigation';
+import CommonButton from '../../UI/CommonButton/CommonButton';
+import { useAppSelector } from '../../redux/store';
 
 export default function Header() {
-  const { pathname } = useLocation();
+  const token = useAppSelector(state => state.user.user.token);
+  console.log(token)
+  return (    
+    <header className='header'>
+      <div className="container">
+      <NavLink to={Path.HOME}>
+        <img src={logo} alt="Logo" />
+      </NavLink>
+      <div className="links">
+        <NavLink to='/'>О нас</NavLink>
+        <NavLink to='/'>Меню тестов</NavLink>
+        <NavLink to='/'>Резюме</NavLink>
+      </div>
+      <div className="controls">
+      {token === null ? (
+        <>
+          <NavLink to={Path.AUTH}>
+            <CommonButton variant='outline'>Войти</CommonButton>
+          </NavLink>
+          <NavLink to={Path.REGISTRATION}>
+            <CommonButton variant='primary'>Регистрация</CommonButton>
+          </NavLink> 
+        </>
+      ) : (
+        <>
+          <NavLink to={Path.PROFILE_SETTINGS}>
+            <CommonButton variant='outline'>Настройки</CommonButton>
+          </NavLink>
+          <CommonButton variant='primary'>Выйти</CommonButton>
+        </>
+      )}
+        
 
-  return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.inner}>
-          <div className={styles.logo}>
-            {pathname === Path.HOME ? (
-              <img src={logo} alt="Logo" />
-            ) : (
-              <NavLink to={Path.HOME}>
-                <img src={logo} alt="Logo" />
-              </NavLink>
-            )}
-          </div>
-          <div className={styles.nav}>
-            <Navigation />
-          </div>
-        </div>
+      </div>
       </div>
     </header>
   );
