860a4c9545a1f2b31ac6c6dbb98d37cac463ff73
lansyfonseka <twinkstriker@gmail.com>
2024-08-18 16:33:49 +0300

    fix: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,40 +6,37 @@ import CommonButton from '../../UI/CommonButton/CommonButton';
 import { useAppSelector } from '../../redux/store';
 
 export default function Header() {
-  const token = useAppSelector(state => state.user.user.token);
-  console.log(token)
-  return (    
-    <header className='header'>
+  const token = useAppSelector((state) => state.user.user.token);
+  return (
+    <header className="header">
       <div className="container">
-      <NavLink to={Path.HOME}>
-        <img src={logo} alt="Logo" />
-      </NavLink>
-      <div className="links">
-        <NavLink to='/'>О нас</NavLink>
-        <NavLink to='/'>Меню тестов</NavLink>
-        <NavLink to='/'>Резюме</NavLink>
-      </div>
-      <div className="controls">
-      {token === null ? (
-        <>
-          <NavLink to={Path.AUTH}>
-            <CommonButton variant='outline'>Войти</CommonButton>
-          </NavLink>
-          <NavLink to={Path.REGISTRATION}>
-            <CommonButton variant='primary'>Регистрация</CommonButton>
-          </NavLink> 
-        </>
-      ) : (
-        <>
-          <NavLink to={Path.PROFILE_SETTINGS}>
-            <CommonButton variant='outline'>Настройки</CommonButton>
-          </NavLink>
-          <CommonButton variant='primary'>Выйти</CommonButton>
-        </>
-      )}
-        
-
-      </div>
+        <NavLink to={Path.HOME}>
+          <img src={logo} alt="Logo" />
+        </NavLink>
+        <div className="links">
+          <NavLink to="/">О нас</NavLink>
+          <NavLink to="/">Меню тестов</NavLink>
+          <NavLink to="/">Резюме</NavLink>
+        </div>
+        <div className="controls">
+          {token === null ? (
+            <>
+              <NavLink to={Path.AUTH}>
+                <CommonButton variant="outline">Войти</CommonButton>
+              </NavLink>
+              <NavLink to={Path.REGISTRATION}>
+                <CommonButton variant="primary">Регистрация</CommonButton>
+              </NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink to={Path.PROFILE_SETTINGS}>
+                <CommonButton variant="outline">Настройки</CommonButton>
+              </NavLink>
+              <CommonButton variant="primary">Выйти</CommonButton>
+            </>
+          )}
+        </div>
       </div>
     </header>
   );
